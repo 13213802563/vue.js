52d07a52139a730ce8a6bdaaf4909b17a49c2416
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-23 23:30:50 +0800

    Add files via upload

---
+++ vueBase/src/components/Model.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="event">
+    <ul>
+      <li v-on:click.once="get(name)"  v-for="name in names">{{name}}</li>
+    </ul>
+    <input type="text" v-on:keyup=''>
+  </div>
+</template>
+<script>
+export default {
+   name: 'Model',
+   data(){
+  	return{
+  		names:["iwen","ime","ice"],
+  	}
+  },
+  methods:{
+  	get(data){
+  		console.log(this)
+  	}
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
