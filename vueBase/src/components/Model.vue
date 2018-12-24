d5d8d543d569c2d09096703b6ccadf5c756bf829
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-24 23:47:23 +0800

    Update Model.vue

--- vueBase/src/components/Model.vue
+++ vueBase/src/components/Model.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="event">
     <ul>
-      <li v-on:click.once="get(name)"  v-for="name in names">{{name}}</li>
+      <li v-on:click.once="getItemInfo(name)"  v-for="name in names">{{name}}</li>
     </ul>
-    <input type="text" v-on:keyup=''>
+    <input type="text" v-on:keyup.enter='getkeyInfo'>
+    <button v-on:click="pushArr">按钮</button>
+    <button>我要奇数</button>
   </div>
 </template>
 <script>
@@ -12,14 +14,28 @@ export default {
    data(){
   	return{
   		names:["iwen","ime","ice"],
+//		nums:[1,2,3,4,5,6,7]
   	}
   },
   methods:{
-  	get(data){
-  		console.log(this)
-  	}
+  	getItemInfo(data){
+     console.log(data);
+  	},
+  	getkeyInfo(event){
+  		console.log(event);
+  	},
+  	pushArr(){
+  	  this.names.push("frank");
+//	  this.name.concat(['frank']);
+  	},
+//	getOdd(){
+//		this.nums.filter(function(num){
+//		 if(num%2!==0){
+//		 this.wantNums.push(num);
+//		 }
+//		})
+//	}
   }
-
 }
 </script>
 
