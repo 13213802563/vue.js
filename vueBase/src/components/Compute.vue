050b86bee17edcf2d4342ac2fcb34c0ff9c93fbf
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-24 23:45:53 +0800

    Add files via upload

---
+++ vueBase/src/components/Compute.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="com">
+		{{message.split('').reverse().join('')}}
+		{{msgRe}}
+		<input type="text" v-model.lazy="msg">
+		<p>{{msg}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+		el:'#com',
+		data(){
+          return{
+           	message:'hello',
+           	msg:'guojun'
+     	 }
+       },
+       watch:{
+       		msg:function(data){
+//				console.log(data);
+             setTimeout(()=>{
+
+             	this.Message=data;
+             },1000)
+			}
+       },
+		computed:{
+			msgRe(){
+				return this.message.split('').reverse().join('');
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
