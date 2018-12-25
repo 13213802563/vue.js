02a8da1f4a99a442742e24f62b680a2da1e70236
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-25 23:30:21 +0800

    Add files via upload

---
+++ vueBase/src/components/Parent.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+<!--	     父亲-->
+		<Son title="传递数据"/>
+		<input type="text"  v-model="lifemsg">
+		<Son :title="msg"  :lifemsg="lifemsg"  :num='num' :objdata='myobj'/>
+		<button @click="changeMsg">按钮</button>
+	</div>
+</template>
+<script>
+	import Son from './Son'
+	
+	export default{
+	name:'pa',
+	data(){
+		return{
+			msg:'我是活的数据',
+			lifemsg:'hello',
+			num:10,
+			myobj:{
+				name:'iwen',
+				age:20
+			}
+		}
+	},
+	components:{
+		Son
+	},
+	methods:{
+		changeMsg(event){
+			this.msg="修改后的数据";
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vueBase/src/components/Son.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		儿子:{{title}}--{{lifemsg}}--{{num*2}}--{{myobj}}
+	</div>
+</template>
+
+<script>
+	
+ export default{
+	name:'son',
+	data(){
+		return{
+			
+		}
+	},
+//	props:['title','lifemsg','num']
+//  props:{
+//  	title:[String,Number],
+//  	lifemsg:String,
+//  	num:Number
+//  }
+    props:{
+    	title:[String,Number],
+    	lifemsg:{
+    		type:String,
+    		required:true
+    	},
+    	num:{
+    		type:Number,
+    		default:5
+    	},
+    	myobj:{
+	    	type:Object,
+	    	default:function(){
+	    		return {
+	    			name:'未知',
+	    			age:10
+	    		}
+	    	}
+    	}
+    }
+  }
+</script>
+
+<style  scoped>
+</style>
