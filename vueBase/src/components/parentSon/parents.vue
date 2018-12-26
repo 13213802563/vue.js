6cbd8f22f28a02349ae0fec00708f3de34b865ff
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-26 23:57:43 +0800

    Add files via upload

---
+++ vueBase/src/components/parentSon/parents.vue
@@ -0,0 +1,39 @@
+<template>
+   <div>
+    	 父组件
+     <input type='text' v-model="num">
+   	 <son @sendmsg="getMsg" :num="num"/>
+   	 {{info}}
+   </div>
+</template>
+
+<script>
+	import son from "./son"
+	
+	export default{
+		name:'parent',
+		data(){
+			return{
+				info:'nihao',
+				num:10
+			}
+	},
+	computed:{
+		getNum(){
+			return this.num-0;
+		}
+		
+	},
+	components:{
+		son
+	},
+	methods:{
+		getMsg(data){
+			this.info=data;
+		}
+	}
+}
+</script>
+
+<style>
+</style>
---
+++ vueBase/src/components/parentSon/son.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		子组件
+	
+		<button @click="sendMsg">传递</button>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"son",
+		data(){
+			return{
+				msg:'我是子组件数据'
+			}
+		},
+		props:{//父传子
+			num:{
+				type:Number,
+				default:5
+			}
+		},
+		computed:{
+			addNum(){
+				return this.num*5;
+			}
+		},
+		methods:{
+			sendMsg(event){
+				//参数1：key,参数2：数据
+//				this.$emit('sendmsg',this.msg);
+                this.$emit('sendmsg',this.addNum);//子传父
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
