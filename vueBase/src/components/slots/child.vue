a10534ae28a23ee323a199a4cbeac4efc4ef512e
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-27 23:56:26 +0800

    Add files via upload

---
+++ vueBase/src/components/slots/child.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="child">
+		子组件	
+		<slot  name="s1"></slot>
+		<br/>
+		<slot name='s2'  text='我是数据传递'></slot>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"son",
+		data(){
+			return{
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.ptest{
+		color:red;
+	}
+</style>
---
+++ vueBase/src/components/slots/parent.vue
@@ -0,0 +1,35 @@
+<template>
+   <div>
+    	 父组件
+    	 <child>
+     	  <div slot='s1'>
+    	 	<!--<p class="ptest">我是插槽</p>   //会插入到子组件中slot中-->
+    	 	<p slot="s1" class="ptest">我是插槽1</p>
+    	 	<p slot="s2" class="ptest">我是插槽2</p>
+    	 	</div>
+             <template slot='s2' slot-scope='key'>////slot-scope
+             	<p class="ptest">
+             		{{key.text}}
+             	</p>
+             </template>
+    	 </child>
+   </div> 
+</template>
+
+<script>
+	import child from "./child"
+	
+	export default{
+		name:'parent',
+		data(){
+			return{
+			}
+	},
+	components:{
+		child
+	}
+}
+</script>
+
+<style>
+</style>
