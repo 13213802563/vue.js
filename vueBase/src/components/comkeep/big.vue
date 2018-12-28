c3f27313c5fd09ff458c6239cec700b609fdec2b
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-28 23:44:22 +0800

    Add files via upload

---
+++ vueBase/src/components/comkeep/big.vue
@@ -0,0 +1,40 @@
+<template>
+	<div> 
+	  big
+	  <component :is='currentView'></component>
+	  <button @click="changeView">切换组件</button> 
+	</div>
+</template>
+import small1 from './small1'
+import small2 from './small2'
+<script>
+	export default{
+		name:'big',
+		data(){
+			return{
+			  currentView:'small1',
+			  flag:true
+			}
+		},
+		components:{
+			small1,
+			small2
+		},
+		methods:{
+			changeView(){
+				if(this.flag){
+					this.currentView='small1';
+					this.flag=false;
+				}
+				else{
+					this.currentView='small2';
+					this.flag=true;
+				}
+			}
+		}
+		
+	}
+</script>
+
+<style scoped>
+</style>
---
+++ vueBase/src/components/comkeep/small2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{msg}}
+    <button @click="changeinfo">changeinfo</button>
+  </div>
+</template>
+
+<script>
+	export default{
+		name:'small2',
+		data(){
+			return{
+				msg:'small2'
+			}
+		},
+		methods:{
+			changeinfo(){
+				this.msg='small11111';
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
