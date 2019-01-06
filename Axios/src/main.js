fa466fc7e3c03ae736fb1e2c7686ac825df0f418
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-01-06 23:34:50 +0800

    Update main.js

--- Axios/src/main.js
+++ Axios/src/main.js
@@ -1,14 +1,35 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import Axios from 'axios'
+import qs from 'qs'
 
 Vue.prototype.$axios = Axios
+//跨域适配
+Vue.prototype.HOST = '/api'
+//全局配置
+// Axios.defaults.baseURL = 'http://www.wwtliu.com';
+// Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+//添加请求拦截器
+Axios.interceptors.request.use(function(config){
+	  if(config.method=='post'){
+	  	config.data=qs.stringify(config.data);
+	  }
+	  return config;
+},function(error){
+	 return Promise.reject(error);
+});
+//添加响应拦截器
+Axios.interceptors.response.use(function(response){
+	return response;
+},function(error){
+	return Promise.reject(error);
+});
+
+
 new Vue({
 	el: '#app',
 	components: {
