89516981023080b135c35987eb28873aecb26c94
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-01-13 21:14:21 +0800

    Update index.js

--- vueRouter/src/router/index.js
+++ vueRouter/src/router/index.js
@@ -2,18 +2,51 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
 import HelloIwen from '@/components/HelloIwen'
+import Index from '@/components/index'
+import Course from '@/components/course'
+import Master from '@/components/master'
+import Java from "@/components/pages/java"
+import Web from "@/components/pages/web"
 
 Vue.use(VueRouter)
 
-export default new  VueRouter({
-	routes:[
-	  {
-	  	path:'/helloWorld',
-	  	component:HelloWorld  
-	  },
-	  {
-	  	path:'/HelloIwen',
-	  	component:HelloIwen
-	  } 
-	 ]
-});
+// export default new  VueRouter({
+// 	routes:[
+// 	  {
+// 	  	path:'/helloWorld',
+// 	  	component:HelloWorld  
+// 	  },
+// 	  {
+// 	  	path:'/HelloIwen',
+// 	  	component:HelloIwen
+// 	  } 
+// 	 ]
+// });
+export default new Router({
+	mode: "history",
+	linkActiveClass: "active",
+	routes: [{
+		path: '/',
+		name: 'index',
+		component: Index
+	}, {
+		path: '/course',
+		name: 'course',
+		component: Course,
+		// 默认进来重定向到java页面
+		redirect: "/course/java",
+		children: [{
+			path: "java",
+			name: "java",
+			component: Java
+		}, {
+			path: "web",
+			name: "web",
+			component: Web
+		}]
+	}, {
+		path: '/master/:count',
+		name: 'master',
+		component: Master
+	}]
+})
