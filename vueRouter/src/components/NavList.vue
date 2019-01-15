b16d47612a83c2c3d046a32b92840bff06722f79
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-01-15 23:16:20 +0800

    Update NavList.vue

--- vueRouter/src/components/NavList.vue
+++ vueRouter/src/components/NavList.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
 	   <ul>
-	     <li><router-link  to='/'>首页</router-link></li>
+	     <li><router-link   to='/' exact>首页</router-link></li>
 	     <li><router-link  to='/course'>课程</router-link></li>
-	     <li><router-link to='/master'>专家</router-link></li>
+	     <li><router-link :to="{name:'master',params:{count:100,type:obj}}">专家</router-link></li>
 	   </ul>
 	</div>
 </template>
