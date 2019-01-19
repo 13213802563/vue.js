34cdaa8b150e2e7b3fea97bbb05194ddd3b71e8d
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-01-19 23:49:13 +0800

    Update main.js

--- Element/src/main.js
+++ Element/src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import Axios from "axios"
+import VueLazyload from 'vue-lazyload'
+
+
 import 'swiper/dist/css/swiper.css'
+
 import {
 	Button, 
 	Select,
@@ -17,8 +22,18 @@ Vue.component(Button.name, Button);
 Vue.component(Select.name, Select);
 Vue.use(Carousel);
 Vue.use(CarouselItem);
-Vue.use(VueAwesomeSwiper)
+Vue.use(VueAwesomeSwiper);
+Vue.use(VueLazyload)
+//Vue.use(VueLazyload, {
+//preLoad: 1.3,
+//error: '../assets/error.jpg',
+//loading: '../assets/loadding.jpg',
+//attempt: 1,
+//// the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
+//listenEvents: [ 'scroll' ]
+//})
 
+Vue.prototype.$axios = Axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
