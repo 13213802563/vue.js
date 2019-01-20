eb839ed39cf09aa82b6b5f6bb100aeb021b2f0e3
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-01-20 23:47:19 +0800

    Add files via upload

---
+++ project11/src/pages/details.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		详情
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'details',
+		data(){
+			return{
+				
+			}
+		},
+		components{
+			
+		}
+	}
+</script>
+
+<style scoped>
+</style>
