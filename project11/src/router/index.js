cf39cc4b10121e6e60d394c57e9e868f93862bee
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-01-22 23:46:31 +0800

    Update index.js

--- project11/src/router/index.js
+++ project11/src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import layout from '@/pages/layout'
-
+import Layout from "@/pages/layout"
+import Details from "@/pages/details"
+import Car from "@/pages/details/car"
+import Hill from "@/pages/details/hill"
+import Earth from "@/pages/details/earth"
+import Loud from "@/pages/details/loud"
 
 Vue.use(Router)
 
@@ -9,8 +13,37 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'layout',
-      component: layout
+      name: 'Layout',
+      component: Layout
+    },
+    {
+  		path:"/details",
+  		name:"details",
+  		component:Details,
+      redirect:"/details/earth",
+      children:[
+        {
+          name:"car",
+          path:"car",
+          component:Car
+        },
+        {
+          name:"Hill",
+          path:"hill",
+          component:Hill
+        },
+        {
+          name:"Earth",
+          path:"earth",
+          component:Earth
+        },
+        {
+          name:"Loud",
+          path:"loud",
+          component:Loud
+        }
+      ]
     }
   ]
 })
+
