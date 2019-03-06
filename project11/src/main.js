a5bfd9f11e5239850a19ee5e2c2066f137f94de8
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-03-06 23:30:58 +0800

    Update main.js

--- project11/src/main.js
+++ project11/src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Axios from "axios" 
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
-
+import store from "./store"
 Vue.use(VueAwesomeSwiper)
+
+Vue.prototype.$axios=Axios
+
 
 Vue.config.productionTip = false;
 
@@ -15,7 +19,7 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
-
