4c5eb82b981a68cb01cb3e1b129a4a73be899b35
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2019-03-28 23:24:39 +0800

    Add files via upload

--- shopping/src/views/cart.vue
+++ shopping/src/views/cart.vue
@@ -2,15 +2,19 @@
 	<div id="main" class="hander-car">
 		<div class="store-content">
 			<div class="cart-box">
+				
 				<div class="title">
 					<h2>购物清单</h2>
 				</div>
+				
 				<div class="cart-inner">
 					<div v-if="count<=0" class="empty-label">
 						<h3>您的购物车中还没有商品</h3>
 						<router-link to="/" class="link">现在选购</router-link>
 					</div>
+					
 					<div v-else>
+						<!--表格头部-->
 						<div class="cart-table-title">
 							<span class="name">商品信息</span>
 							<span class="operation">操作</span>
@@ -18,18 +22,23 @@
 							<span class="num">数量</span>
 							<span class="price">单价</span>
 						</div>
+						
 						<div class="cart-table">
 							<div class="cart-group">
-								<!--购物列表-->
+								<!--购物列表    carPanelData为购物车数据信息-->
 								<div class="cart-top-items" v-for="(item,index) in carPanelData">
+									
 									<div class="cart-items">
+										<!--选中状态:class="{' checkbox-on':item.checked}"-->
 										<div class="items-choose">
 											<span class="blue-checkbox-new" :class="{' checkbox-on':item.checked}" @click="checkGoodsHandle(item.sku_id)"><a></a></span>
 										</div>
+										<!--渲染图片-->
 										<div class="items-thumb">
 											<img :src="item.ali_image+'?x-oss-process=image/resize,w_80/quality,Q_100/format,webp'">
 											<a href="javascript:;" target="_blank"></a>
 										</div>
+									<!--	商品信息：-->
 										<div class="name hide-row">
 											<div class="name-table">
 												<a href="javascript:;" target="_blank">{{item.title}}</a>
@@ -38,10 +47,13 @@
 												</ul>
 											</div>
 										</div>
+								    <!--操作：删除操作-->
 										<div class="operation">
 											<a class="items-delete-btn" @click="delCarPanelHandle(item.sku_id)"></a>
 										</div>
+										<!--小计-->
 										<div class="subtotal">¥ {{item.price*item.count}}.00</div>
+										<!--数量-->
 										<div class="item-cols-num">
 											<div class="select js-select-quantity">
 												<span class="down" :class="{' down-disabled':item.count<=1}" @click="subCarPanelHandle(item.sku_id)">-</span>
@@ -50,43 +62,55 @@
 	
 											</div>
 										</div>
+										<!--价格-->
 										<div class="price">¥ {{item.price}}.00</div>
 									</div>
 								</div>
 							</div>
 						</div>
+						
 					</div>
 				</div>
 				<div v-if="count>0" class="cart-bottom-bg fix-bottom">
+					
 					<div class="cart-bar-operation">
-						<div>
+						<div>	
+							<!--全选   :class="{'checkbox-on':allChecked}获得computed中计算的值-->
 							<div class="choose-all js-choose-all">
 								<span class="blue-checkbox-new" :class="{'checkbox-on':allChecked}" @click="allGoodsCheckHandle(allChecked)"><a></a></span> 全选
 							</div>
+							<!--删除所有选中的商品-->
 							<div class="delete-choose-goods" @click="delCheckGoodsHandle">删除选中的商品</div>
+							
 						</div>
 					</div>
+					<!--//结算-->
 					<div class="shipping">
 						<div class="shipping-box">
+							
 							<div class="shipping-total shipping-num">
 								<h4 class="">
 									已选择 <i>{{checkedCount}}</i> 件商品
 								</h4>
+								
 								<h5>
 									共计 <i >{{count}}</i> 件商品
 								</h5>
 							</div>
+							
 							<div class="shipping-total shipping-price">
 								<h4 class="">
   								应付总额：<span>￥</span><i >{{checkedTotle}}</i> 
-  							</h4>
+  							   </h4>
 								<h5 class="shipping-tips">
 									应付总额不含运费
 								</h5>
 							</div>
+							
 						</div>
 						<span class="jianguo-blue-main-btn big-main-btn js-checkout" :class="{'disabled-btn':checkedCount<=0}" @click="checkOutHandle"><a>现在结算</a></span>
 					</div>
+					
 				</div>
 			</div>
 		</div>
@@ -95,6 +119,7 @@
 
 <script>
   export default {
+  	//需要计算写在computed中
     computed: {
     //拿到购物车的数据
       carPanelData () {
@@ -129,6 +154,7 @@
       checkGoodsHandle (id) {
         this.$store.commit('checkGoods',id)
       },
+      //全选控制单个
       allGoodsCheckHandle (checked) {
         this.$store.commit('allGoodsCheck',checked)
       },
